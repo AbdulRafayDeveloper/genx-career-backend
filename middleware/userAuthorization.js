49882da60683ce58c0fcf7ab4460db17bf8d1948
Abdul Rafay <amirrafay135@gmail.com>
2025-02-12 02:14:04 -0800

    add middleware for user

--- middleware/userAuthorization.js
+++ middleware/userAuthorization.js
@@ -36,4 +36,37 @@ const authenticateLoginToken = async (req, res, next) => {
   }
 };
 
-export { authenticateLoginToken };
+const userAuthenticateLoginToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return badRequestResponse(res, "Authentication token is required", null);
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return notFoundResponse(res, "Authentication token is not provided", null);
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = { _id: decoded.id, role: decoded.role };
+
+    if (req.user.role == "admin") {
+      return unauthorizedResponse(res, "Access denied. User role required.", null);
+    }
+
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return unauthorizedResponse(res, "Expired token, log in again");
+    } else if (error.name === "JsonWebTokenError") {
+      return unauthorizedResponse(res, "Invalid token, log in again");
+    } else {
+      return serverErrorResponse(res, "An unexpected error occurred");
+    }
+  }
+};
+
+export { authenticateLoginToken, userAuthenticateLoginToken };
--- routes/cvMatching/cvMatchingRoutes.js
+++ routes/cvMatching/cvMatchingRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { userCvMatching, getOneCvMatcher, getAllCvMatchers, deleteCvMatcher, exportCvMatchersToExcel } from "../../controllers/cvMatching/cvMatchingController.js";
 import { upload } from "../../helpers/pdfStoragehelpers.js";
-import { authenticateLoginToken } from "../../middleware/userAuthorization.js";
+import { authenticateLoginToken, userAuthenticateLoginToken } from "../../middleware/userAuthorization.js";
 
 const router = express.Router();
 
-router.post("/cv-matching", upload.single("file"), userCvMatching);
+router.post("/cv-matching", userAuthenticateLoginToken, upload.single("file"), userCvMatching);
 router.get("/cv-matchers", authenticateLoginToken, getAllCvMatchers);
 router.get("/cv-matching/:id", getOneCvMatcher);
 router.delete("/cv-matching/:id", authenticateLoginToken, deleteCvMatcher);

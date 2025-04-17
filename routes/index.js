a46a76ed9cfa76ec19256a79c848bc1bf9ada1a2
Geeky <124719983+arooba-zaman1211@users.noreply.github.com>
2025-04-17 23:43:08 +0500

    cv generation module added

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,26 @@
-import express from 'express';
-import userAuthRoutes from './users/authRoutes.js';
-import userRoutes from './users/usersRoutes.js';
-import contactUsRoutes from './users/contactRoutes.js';
-import jobsRoutes from './jobs/jobsRoutes.js';
-import cvMatchingRoutes from './cvMatching/cvMatchingRoutes.js';
-import adminDashboardRoutes from './dashboard/adminDashboardRoutes.js';
+import express from "express";
+import userAuthRoutes from "./users/authRoutes.js";
+import userRoutes from "./users/usersRoutes.js";
+import contactUsRoutes from "./users/contactRoutes.js";
+import jobsRoutes from "./jobs/jobsRoutes.js";
+import cvMatchingRoutes from "./cvMatching/cvMatchingRoutes.js";
+import adminDashboardRoutes from "./dashboard/adminDashboardRoutes.js";
+import cvCreation from "./cvCreation/cvRoutes.js";
 
 const router = express.Router();
 
 // Just for Testing
 router.get("/", (req, res) => {
-    res.send('Main project backend is working now')
-})
+  res.send("Main project backend is working now");
+});
 
 // Attach all routes here
-router.use('/api', userAuthRoutes);
-router.use('/api', userRoutes);
-router.use('/api', contactUsRoutes);
-router.use('/api', jobsRoutes);
-router.use('/api', cvMatchingRoutes);
-router.use('/api', adminDashboardRoutes);
+router.use("/api", userAuthRoutes);
+router.use("/api", userRoutes);
+router.use("/api", contactUsRoutes);
+router.use("/api", jobsRoutes);
+router.use("/api", cvMatchingRoutes);
+router.use("/api", adminDashboardRoutes);
+router.use("/api", cvCreation);
 
 export default router;

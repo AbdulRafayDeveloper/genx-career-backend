29092e386b1c1214e364c0626547947856a6434d
Abdul Rafay <amirrafay135@gmail.com>
2025-07-05 13:39:33 +0000

    testing 2.9 - fix the cv creation api

--- helpers/cvCreationHelper/pdfshiftHelper.js
+++ helpers/cvCreationHelper/pdfshiftHelper.js
@@ -1,27 +1,9 @@
-// import axios from "axios";
-
-// export async function convertHtmlToPdfBuffer(htmlContent) {
-//     const response = await axios.post(
-//         "https://api.pdfshift.io/v3/convert/pdf",
-//         {
-//             source: htmlContent,
-//         },
-//         {
-//             auth: {
-//                 username: "sk_a00e969bad3b624de18ec0f78e69cee41de7dbff",
-//                 password: "",
-//             },
-//             responseType: "arraybuffer",
-//         }
-//     );
-
-//     return Buffer.from(response.data);
-// }
-
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 export async function convertHtmlToPdfBuffer(htmlContent) {
-    const api_key = "sk_83f820c8e4ec838d86ab13e32edb746a3ee8f657";
+    const api_key = process.env.PDFSHIFT_API_KEY;
 
     const response = await axios.post(
         "https://api.pdfshift.io/v3/convert/pdf",
@@ -30,7 +12,7 @@ export async function convertHtmlToPdfBuffer(htmlContent) {
         },
         {
             headers: {
-                'X-API-Key': "sk_83f820c8e4ec838d86ab13e32edb746a3ee8f657"
+                'X-API-Key': api_key
             },
             responseType: "arraybuffer",
         }
